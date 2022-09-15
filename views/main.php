70f50c2d5ab904e5f84c352cb148f0fe52f0d386
EHE781 <emanuel.hegedus18@gmail.com>
2022-09-15 17:04:05 +0200

    Test push

--- views/main.php
+++ views/main.php
@@ -1,11 +1,14 @@
-<?php require_once __DIR__."/../head.php"?>
+<?php require_once __DIR__ . "/../head.php" ?>
 <html>
-    <body class="mainBody">
-        <h1>Body</h1>
-    </body>
+
+<body class="mainBody">
+    <h1>Header1</h1>
+    <p>Paragraph</p>
+</body>
+
 </html>
 <script>
-    $(document).ready(function(){
-        
-    });
+$(document).ready(function() {
+
+});
 </script>
