f8f22f13a9dbb9ca6347ec76b01030306abd7f53
Xavo0712 <xavmat2001@gmail.com>
2022-09-15 15:40:02 +0200

    Project body
    Jquery and Bootstrap installation

---
+++ head.php
@@ -0,0 +1,7 @@
+<head>
+    <link src="../css/bootstrap.css"/>
+    <link src="../styles/styles.css"/>
+
+    <script src="../js/jquery-3.6.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</head>
---
+++ views/main.php
@@ -0,0 +1,11 @@
+<?php require_once __DIR__."/../head.php"?>
+<html>
+    <body class="mainBody">
+        <h1>Body</h1>
+    </body>
+</html>
+<script>
+    $(document).ready(function(){
+        
+    });
+</script>
