04a755729572f0cc3ac0296fadaa61c011e46809
Xavo0712 <xavmat2001@gmail.com>
2022-09-19 19:12:13 +0200

    change icon href

--- views/header.php
+++ views/header.php
@@ -1,6 +1,6 @@
 <?php require_once __DIR__ . "/menu.php" ?>
 <div class="header col-12">
-    <div id="navOpener" onclick="openNav()"><i class="icon col-lg-1 fa-solid fa-bars fa-3x"></i></div>
+    <a href="#" id="navOpener" onclick="openNav()"><i class="icon col-lg-1 fa-solid fa-bars fa-3x"></i></a>
     <h1 class="col-lg-10">This is my custom header title</h1>
     <img class="col-lg-1" src="./assets/img/myimg.png"></i>
 </div>
