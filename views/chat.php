96be8e0d90e38d02b01a4a1ea09b73ca8a786544
Xavo0712 <xavmat2001@gmail.com>
2022-09-23 00:48:01 +0200

    Chat half implemented

---
+++ views/chat.php
@@ -0,0 +1,20 @@
+<div id="myChat" class="chat container">
+  <div class="panel panel-default border">
+    <div class="panel-heading chat-header col-12">
+      <i class="fa-solid fa-arrow-left text-center"></i>
+      <h5 class="text-center">User198</h5>
+      <i class="fa-solid fa-chevron-down text-center"></i>
+    </div>
+    <div class="panel-body chat-body">
+      <div class="sent-message">
+        <p>Hola! Cómo fue la tarde?? Bien?</p>
+      </div>
+      <div class="received-message">
+        <p>Hola! Ahora que lo mencionas, fue bastante mejor de lo que esperaba.</p>
+      </div>
+    </div>
+    <div class="panel-footer chat-footer">
+      <input id="chatWriter" placeholder="Escriba un mensaje"></input>
+    </div>
+  </div>
+</div>
--- views/main.php
+++ views/main.php
@@ -1,6 +1,7 @@
 <html>
 <div style="min-width: 100%">
     <?php require_once __DIR__ . "/header.php" ?>
+    <?php require_once __DIR__ . "/chat.php" ?>
     <body class="mainBody">
         <h1>Header1</h1>
         <p>Paragraph</p>
