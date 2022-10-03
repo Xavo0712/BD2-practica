2a282ed44ee3dfda405f074d3c9269ad8c56c08e
Xavo0712 <xavmat2001@gmail.com>
2022-10-03 17:37:09 +0200

    Responsive css

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <head>
     <link rel="stylesheet" href="./assets/css/bootstrap.css"/>
     <link rel="stylesheet" href="./assets/styles/styles.css"/>
+    <link rel="stylesheet" href="./assets/styles/responsive.css"/>
 
     <script src="./assets/js/jquery-3.6.1.min.js"></script>
     <script src="./assets/js/bootstrap.min.js"></script>
