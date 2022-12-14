053c6ab05b5afc4a0177bd4ce0c512bf2d238453
Xavo0712 <61181589+Xavo0712@users.noreply.github.com>
2022-12-14 21:20:06 +0100

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -2,13 +2,29 @@
     <?php require_once __DIR__ . "/header.php" ?>
     <?php require_once __DIR__ . "/chat.php" ?>
 
+    <?php
+    $loggedUser = $_COOKIE['user']; //paco as user for test purpouse
+    $loggedUserInfo = DB::run("SELECT * FROM usuari WHERE idUser = ?", [$loggedUser])->fetchAll(PDO::FETCH_ASSOC)[0];
+
+    $allUserPosts = DB::run("SELECT link AS img, text AS text, publicacio.idPub FROM publicacio 
+        JOIN r_reenv ON (r_reenv.idPub = publicacio.idPub AND r_reenv.idUser = ?) OR (publicacio.idUser = ? AND publicacio.idHist IS NULL);", [$loggedUser, $loggedUser])->fetchAll(PDO::FETCH_ASSOC);
+
+    $allUserStoriesIds = DB::run("SELECT idHist FROM historia WHERE idUser = ?", [$loggedUser])->fetchAll(PDO::FETCH_ASSOC);
+    $allUserStories = array();
+    foreach ($allUserStoriesIds as $id) {
+        $postsInStory = DB::run("SELECT link AS img, text AS text, idPub, idHist FROM publicacio
+            WHERE idHist = ?", [$id['idHist']])->fetchAll(PDO::FETCH_ASSOC);
+        array_push($allUserStories, $postsInStory);
+    }
+    ?>
+
     <body class="mainBody">
         <div class="row">
             <div class="panel panel-default col-lg-6 profileBlock">
-                <img style="margin-left:33%;" src="https://cdn-icons-png.flaticon.com/512/235/235359.png" width="300px" height="300px" />
+                <img style="margin-left:33%; margin-top:20px;" class="userPic" src=<?php echo "\"" . $loggedUserInfo['imagen'] . "\"" ?> width="300px" height="300px" />
                 <div class="row">
-                    <p class="profileInfo">@CapybaraGigaChad</p>
-                    <p class="profileInfo">Panita Manu</p>
+                    <p class="profileInfo username">@<?php echo $loggedUserInfo['username'] ?></p>
+                    <p class="profileInfo"><?php echo $loggedUserInfo['nom'] ?></p>
                 </div>
                 <div class="row">
                     <div id="postButton" class="col-lg-6 buttonText">
@@ -19,96 +35,46 @@
                     </div>
                 </div>
                 <div class="panel panel-default profilePosts" style="overflow-y:auto; height:800px;">
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
-                    <div class="row post">
-                        <p>Los capybaras me gustan mucho son increibles :)</p>
-                        <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                    </div>
+                    <?php
+                    foreach ($allUserPosts as $post) {
+                        echo "  <div id=\"post" . $post['idPub'] . "\" class=\"row post\">
+                                    <p>" . $post['text'] . "</p>
+                                    <img src=\"" . $post['img'] . "\" max-height=\"200px\" max-width=\"200px\">
+                                </div>";
+                    }
+                    ?>
                 </div>
                 <div class="panel panel-default profileStories" style="display:none; overflow-y:auto; height:800px;">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <a class="carousel-control-prev" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <div class="carousel-inner">
-                            <div class="carousel-item row post">
-                                <p>Los capybaras me gustan mucho son increibles :)</p>
-                                <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                            </div>
-                            <div class="carousel-item row post">
-                                <p>Los capybaras me gustan mucho son increibles :)</p>
-                                <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                            </div>
-                            <div class="carousel-item row post active">
-                                <p>Los capybaras no me gustan mucho no son increibles :)</p>
-                                <img src="https://cdn.discordapp.com/attachments/491278087053377540/1047566527991382016/unknown.png" max-height="200px" max-width="200px">
-                            </div>
-                        </div>
-                        <a class="carousel-control-next" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <a class="carousel-control-prev" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <div class="carousel-inner">
-                            <div class="carousel-item post">
-                                <p>Los capybaras me gustan mucho son increibles :)</p>
-                                <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                            </div>
-                            <div class="carousel-item post">
-                                <p>Los capybaras me gustan mucho son increibles :)</p>
-                                <img src="https://cdn.discordapp.com/attachments/1027641770865078273/1048655138962821260/IMG-20221018-WA0002.jpg" max-height="200px" max-width="200px">
-                            </div>
-                            <div class="carousel-item post active">
-                                <p>Los capybaras no me gustan mucho no son increibles :)</p>
-                                <img src="https://cdn.discordapp.com/attachments/491278087053377540/1047566527991382016/unknown.png" max-height="200px" max-width="200px">
-                            </div>
-                        </div>
-                        <a class="carousel-control-next" id="nextPost" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
+                    <?php
+                    foreach ($allUserStories as $story) {
+                        $primero = false;
+                        echo "<div id=\"story" . $story[0]['idHist'] . "\" class=\"carousel slide\" data-ride=\"carousel\">
+                            <a class=\"carousel-control-prev\" role=\"button\" data-slide=\"prev\">
+                                <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>
+                                <span class=\"sr-only\">Previous</span>
+                            </a>
+                            <div class=\"carousel-inner\">";
+                        foreach ($story as $post) {
+
+                            echo "  <div id=\"post" . $post['idPub'] . "\" class=\"carousel-item row post ".(($primero == false)? "active" : "" )."\">
+                                        <p>" . $post['text'] . "</p>
+                                        <img src=\"" . $post['img'] . "\" max-height=\"200px\" max-width=\"200px\">
+                                    </div>";
+                            $primero = true;
+                        }
+                        echo "</div>
+                                <a class=\"carousel-control-next\" role=\"button\" data-slide=\"next\">
+                                    <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"></span>
+                                    <span class=\"sr-only\">Next</span>
+                                </a>
+                            </div>";
+                    }
+                    ?>
                 </div>
             </div>
         </div>
-    </body>
+</div>
+</body>
 </div>
 
 <script>
